5a0aee7f57fce24b96fba602939198c0306780c1
uposledzony <chrustek123456789@gmail.com>
2020-03-11 13:49:38 +0100

    Added the first feature - printing the name of group and the ID of leader

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Name of group Internet_Explorer | GitHub Leader ID: Kristrox");
     }
 }
