70c9fae11f5de71faf16ea0614c8bca60d95ae15
uposledzony <chrustek123456789@gmail.com>
2020-03-16 23:29:27 +0100

    Added another important message

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //multiplication example
         ArithmeticsMult multiplication = new ArithmeticsMult();
         System.out.println("result of multiplication = " + multiplication.Multiplication(20, 10));
-        //difference example
+        //difference example implemented by Rysiuziom
         ArithmeticsDiff difference = new ArithmeticsDiff();
         System.out.println("result of difference 10 - 5 = " + difference.Difference(10, 5));
         // division
