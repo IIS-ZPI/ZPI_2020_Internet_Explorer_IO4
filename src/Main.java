d308cd9611717cba83414e49f7e751eb706c5be2
jacek.wychowałek <215927@edu.p.lodz.pl>
2020-03-16 23:25:22 +0100

    added second comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Name of group Internet_Explorer | GitHub Leader ID: Kristrox | GitHubID: Kristrox");
         //multiplication example
+        //kom2
         ArithmeticsMult multiplication = new ArithmeticsMult();
         System.out.println("result of multiplication = " + multiplication.Multiplication(20, 10));
         //difference example
