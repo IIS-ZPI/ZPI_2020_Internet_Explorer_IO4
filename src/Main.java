832339879045a7cfb38416f1b36c4c7cfb1ced91
jacek.wychowałek <215927@edu.p.lodz.pl>
2020-03-16 23:24:54 +0100

    added first comment

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("result of multiplication = " + multiplication.Multiplication(20, 10));
         //difference example
         ArithmeticsDiff difference = new ArithmeticsDiff();
+        //kom1
         System.out.println("result of difference 10 - 5 = " + difference.Difference(10, 5));
 	System.out.println(new BasicArithmeticsDivImpl().Division(3,4));
     }
